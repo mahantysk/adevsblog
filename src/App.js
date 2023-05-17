d2c1f67d9f05710c873e0a94672e5c0902f710d5
Sandeep Kumar Mahanty <smahanty@wayfair.com>
2023-05-17 15:08:35 +0530

    Add firebase and circleci

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { Button, Grommet, grommet, Header, Page, PageContent, PageHeader, Text } from 'grommet';
-import { Moon, Sun } from "grommet-icons";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Grommet, grommet } from 'grommet';
 import React, { useState } from "react";
 
 import { deepMerge } from 'grommet/utils';
 
-
-const AppBar = (props) => (
-  <Header
-    background="brand"
-    pad={{ left: "medium", right: "small", vertical: "small" }}
-    elevation="medium"
-    {...props} />
-)
+import Register from "./components/accounts/Register";
+import Login from "./components/accounts/Login";
 
 const theme = deepMerge(grommet, {
   global: {
@@ -28,19 +22,12 @@ function App() {
 
   return (
     <Grommet theme={theme} full themeMode={dark ? 'dark' : 'light'}>
-      <Page>
-        <AppBar>
-          <Text size="large">My App</Text>
-          <Button
-            a11yTitle={dark ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
-            icon={dark ? <Moon /> : <Sun />}
-            onClick={() => setDark(!dark)}
-          />
-        </AppBar>
-        <PageContent>
-          <PageHeader title="Welcome to my blog" />
-        </PageContent>
-      </Page>
+      <Router>
+        <Routes>
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </Grommet>
   );
 }
